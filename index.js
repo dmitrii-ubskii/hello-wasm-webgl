cc1c6998211cb5c83e8cbf65e82b9f1519b80a7a
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-02-23 18:28:58 +0000

    hello triangle (no WASM, just WebGL)

--- index.js
+++ index.js
@@ -19,3 +19,87 @@ fetch("output.wasm").then(res => res.arrayBuffer()).then(function (wasmBytes) {
         wasm.run();
     });
 });
+
+var canvas;
+var gl;
+var vao;
+
+function render() {
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+};
+
+function resizeCanvas() {
+    var width = canvas.clientWidth;
+    var height = canvas.clientHeight;
+    if (canvas.width != width ||
+        canvas.height != height) {
+        canvas.width = width;
+        canvas.height = height;
+        render();
+    }
+}
+
+window.onload = function () {
+    canvas = document.getElementById("canvas");
+
+    gl = canvas.getContext("webgl2");
+
+    var vert = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(
+        vert,
+        `#version 300 es
+        in vec4 position;
+        void main() {
+            gl_Position = position;
+        }`
+    );
+    gl.compileShader(vert);
+
+    var frag = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(
+        frag,
+        `#version 300 es
+        precision highp float;
+        out vec4 outColor;
+        void main() {
+            outColor = vec4(1, 1, 1, 1);
+        }`
+    );
+    gl.compileShader(frag);
+
+    shader = gl.createProgram();
+    gl.attachShader(shader, vert);
+    gl.attachShader(shader, frag);
+    gl.linkProgram(shader);
+    gl.useProgram(shader);
+
+    let positions = new Float32Array(9);
+    positions[0] = -0.7;
+    positions[1] = -0.6;
+    positions[2] =  0.0;
+    positions[3] =  0.7;
+    positions[4] = -0.6;
+    positions[5] =  0.0;
+    positions[6] =  0.0;
+    positions[7] =  0.6;
+    positions[8] =  0.0;
+
+    let pos = gl.getAttribLocation(shader, "position");
+    let buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+
+    vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+    gl.vertexAttribPointer(pos, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(pos);
+
+    resizeCanvas();
+    render();
+}
+
+window.addEventListener('resize', resizeCanvas);
